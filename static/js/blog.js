3e98b786ee6dbf0064aef53f5c633e6ffb3945be
Riya Gunda <rgunda@ncsu.edu>
2024-02-25 10:26:29 -0500

    finished blog

--- static/js/blog.js
+++
@@ -1,6 +0,0 @@
-const dates = document.getElementsByClassName("date")
-for (let date of dates) {
-    const today = new Date();
-    let str = today.toDateString()
-    date.innerHTML = `Date: ${str}`
-}
